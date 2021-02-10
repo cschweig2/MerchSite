044730a021c46502c7e6cc3340e74f15050273ac
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-02-10 14:42:47 -0800

    Add Delete functionality.
    Co-authored-by: Nathan Schrader <nathanschrader@icloud.com>
    Co-authored-by Taylor Delph <taylulzcode@gmail.com>

--- src/components/ItemDetails.js
+++ src/components/ItemDetails.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function ItemDetail(props){
-  const {item} = props;
+  const {item, onClickingDelete} = props;
   return(
     <>
       <h1>Item Detail</h1>
@@ -10,11 +10,14 @@ function ItemDetail(props){
       <p>Description: {item.description}</p>
       <p>${item.price}</p>
       <p>Quantity: {item.quantity}</p>
+      <button onClick={()=> onClickingDelete(item.id) }>Delete Item</button>
       <hr/>
     </>
   )
 }
 ItemDetail.propTypes ={
-  item: PropTypes.object
-}
+  item: PropTypes.object,
+  onClickingDelete: PropTypes.func
+};
+
 export default ItemDetail;
