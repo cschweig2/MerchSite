36409f748e6a02b9ac164c204fd15d788b658550
kevinkirkl3y <kevinmkirkley@gmail.com>
2021-02-15 13:49:34 -0800

    Purposefully passed test for adding item to list.
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>
    Co-authored-by: Agata Kolodziej <agatakolohe@gmail.com>

--- src/reducers/item-list-reducer.js
+++ src/reducers/item-list-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, description, price, quantity, id } = action;
+  switch (action.type) {
+  case 'ADD_ITEM':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        description: description,
+        price: price,
+        quantity: quantity,
+        id: id
+      }
+    });
+    default:
+      return state;
+  }
 };
